7664e1fc06827d40f9e7aaf0c09ab23f4e09f11c
skypeian <skypeian@gmail.com>
2012-08-26 01:30:57 -0700

    bullets will self destruct now after "bulletLife"

--- Scripts/bulletScript.cs
+++ Scripts/bulletScript.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class bulletScript : MonoBehaviour {
 	public int bulletSpeed = 10;
-	public int bulletDamage = 0;
+	public int bulletDamage = 10;
+	public float bulletLife = 3f;
 	public Vector3 bulletDirection;
 	
 	void Start () {
@@ -13,13 +14,15 @@ public class bulletScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.gameObject.transform.Translate(0, 0, -(bulletSpeed * Time.deltaTime));
+		
+		bulletLife -= Time.deltaTime;
+		if (bulletLife <= 0)
+			Destroy(this.gameObject);
 	}
 	
 	void OnCollisionEnter (Collision other){
 		if (other.gameObject.collider){
-			bulletDamage = 5;
-			Destroy(this);
+			Destroy(this.gameObject);
 		}
 	}
-	
 }
